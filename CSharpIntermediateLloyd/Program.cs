f0c627aebe5fcd32d5c1abed3a6d033ff9b277e9
Lloyd <l.h.b.tan@gmail.com>
2020-04-23 23:16:15 +0200

    added stopwatch

--- CSharpIntermediateLloyd/Program.cs
+++ CSharpIntermediateLloyd/Program.cs
@@ -3,16 +3,38 @@ using System.Threading.Channels;
 
 namespace CSharpIntermediateLloyd
 {
-    public class Person
-    {
-        public DateTime Birthdate { get; set; }
-    }
     class Program 
     {
         static void Main(string[] args)
         {
-         //first commit
-         //This is in develop
+            Console.WriteLine("Write  'Start' to start the stopwatch, and 'Stop' to end it.");
+            var stopwatch1 = new Stopwatch();
+            while (true)
+            {
+                var input = Console.ReadLine().ToLower();
+                if (input == "start")
+                {
+                    stopwatch1.Start();
+                }
+                else if (input == "stop")
+                {
+                    var result = stopwatch1.Stop();
+                    if (result == 0)
+                    {
+                        Console.WriteLine("Start the stopwatch first, no time recorded");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Your time is: " + result + " seconds!");
+                    }
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            Console.WriteLine("Program stopped");
         }
     }
 }
---
+++ CSharpIntermediateLloyd/Stopwatch.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace CSharpIntermediateLloyd
+{
+    public class Stopwatch
+    {
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public Boolean IsStarted { get; set; }
+
+        public void Start()
+        {
+            if (!IsStarted)
+            {
+                IsStarted = true;
+                StartTime = DateTime.Now;
+            }
+            else
+            {
+                throw new InvalidOperationException("Stopwatch is already started");
+            }
+        }
+
+        public double Stop()
+        {
+            var diff = TimeSpan.Zero;
+            if (IsStarted)
+            {
+                IsStarted = false;
+                EndTime = DateTime.Now;
+                diff = EndTime - StartTime;
+                
+            }
+            return diff.TotalSeconds;
+        }
+    }
+}
