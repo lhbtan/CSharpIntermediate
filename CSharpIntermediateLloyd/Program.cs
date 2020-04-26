0471b69eced3a390b242c8b3d293f8118a898a9d
Lloyd <l.h.b.tan@gmail.com>
2020-04-26 18:32:51 +0200

    dynamic form namaak

--- CSharpIntermediateLloyd/Program.cs
+++ CSharpIntermediateLloyd/Program.cs
@@ -7,34 +7,8 @@ namespace CSharpIntermediateLloyd
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Write  'Start' to start the stopwatch, and 'Stop' to end it.");
-            var stopwatch1 = new Stopwatch();
-            while (true)
-            {
-                var input = Console.ReadLine().ToLower();
-                if (input == "start")
-                {
-                    stopwatch1.Start();
-                }
-                else if (input == "stop")
-                {
-                    var result = stopwatch1.Stop();
-                    if (result == 0)
-                    {
-                        Console.WriteLine("Start the stopwatch first, no time recorded");
-                    }
-                    else
-                    {
-                        Console.WriteLine("Your time is: " + result + " seconds!");
-                    }
-                }
-                else
-                {
-                    break;
-                }
-            }
-
-            Console.WriteLine("Program stopped");
+            
+           
         }
     }
 }
