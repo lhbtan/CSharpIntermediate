e89e2307b15a796bb654aebfe9eea8f2822473c9
Lloyd <l.h.b.tan@gmail.com>
2020-05-05 11:45:47 +0200

    stack builder

--- CSharpIntermediateLloyd/Program.cs
+++ CSharpIntermediateLloyd/Program.cs
@@ -1,14 +1,24 @@
-﻿using System.Threading.Channels;
+﻿
+using System;
 
 namespace CSharpIntermediateLloyd
 {
+
+
+
     class Program 
     {
         static void Main(string[] args)
         {
-            var text = new Text();
-            text.Width = 100;
-            text.Copy();
+           
+                var stack1 = new Stack();
+                stack1.Push("1234");
+                stack1.Push(123);
+                stack1.Push("34536363");
+                
+                Console.WriteLine(stack1.Pop());
+                Console.WriteLine(stack1.Pop());
+                Console.WriteLine(stack1.Pop());
         }
     }
 }
