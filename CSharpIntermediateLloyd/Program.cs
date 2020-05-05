87822d0e4a4381c82c0ad8670af1698c3af92c7f
Lloyd <l.h.b.tan@gmail.com>
2020-05-05 16:01:34 +0200

    commit

--- CSharpIntermediateLloyd/Program.cs
+++ CSharpIntermediateLloyd/Program.cs
@@ -10,15 +10,7 @@ namespace CSharpIntermediateLloyd
     {
         static void Main(string[] args)
         {
-           
-                var stack1 = new Stack();
-                stack1.Push("1234");
-                stack1.Push(123);
-                stack1.Push("34536363");
-                
-                Console.WriteLine(stack1.Pop());
-                Console.WriteLine(stack1.Pop());
-                Console.WriteLine(stack1.Pop());
+
         }
     }
 }
